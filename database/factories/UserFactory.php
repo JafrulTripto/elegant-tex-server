5ef68cda6b275541716e6fad67833bd0b98e8652
jafrultripto <jafrultripto@gmail.com>
2023-07-11 23:50:07 +0600

    user factory added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->name(),
+            'lastname' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'nid' => fake()->numberBetween(5454821221, 54548210000),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'last_login' => fake()->dateTime('now'),
             'remember_token' => Str::random(10),
         ];
     }
