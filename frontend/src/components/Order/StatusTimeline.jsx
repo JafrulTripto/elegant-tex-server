61558d634a5b2fd24efa4a76eed3597327c7e17d
JafrulTripto <jafrultripto@gmail.com>
2024-07-11 20:08:38 +0800

    Order timeline component updated

--- frontend/src/components/Order/StatusTimeline.jsx
+++ frontend/src/components/Order/StatusTimeline.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
-import {Tag, Timeline} from 'antd';
+import {Empty, Tag, Timeline} from 'antd';
 import {ClockCircleOutlined} from "@ant-design/icons";
 import dayjs from "dayjs";
+import {OrderStatusEnum} from "../../utils/enums/OrderStatusEnum";
+import { Typography } from 'antd';
 
 const OrderTimeline = ({statuses}) => {
+    const { Text } = Typography;
+    const getStatusDetails = (statusId) => {
+        const status = OrderStatusEnum.find(status => status.value === statusId);
+        if (status) {
+            return {
+                label: status.label,
+                color: status.color
+            };
+        }
+        return null;
+    };
     const renderStatus = (user, comment, timestamp) => {
         return (
             <div>
                 <div>{comment}</div>
                 <div>{dayjs(timestamp).format('MMMM Do YYYY, h:mm a')}</div>
-                <div>({user.firstname})</div>
+                <Text strong italic>({user.firstname})</Text>
             </div>
         );
     }
 
+    if (statuses.length === 0) {
+        return <Empty/>;
+    }
+
     const timelineItems = statuses.map((statusChangeItem) => {
-        const { user, comment, created_at } = statusChangeItem;
+        const { user, comment, created_at,status } = statusChangeItem;
         const timestamp = new Date(created_at).getTime();
+        const {label, color} = getStatusDetails(status);
 
         return {
+            label:<Tag color={color}>{label}</Tag>,
             dot: (
                 <ClockCircleOutlined
                     style={{
                         fontSize: '16px',
-                        color: 'red',
+                        color: color,
                     }}
                 />
             ),
