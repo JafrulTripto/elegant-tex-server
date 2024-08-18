2df4327d0906b5e7f658d6309f6b9121020f5cb9
JafrulTripto <jafrultripto@gmail.com>
2024-08-18 22:11:09 +0800

    New card added in user dashboard

--- frontend/src/components/Dashboard/UserDashboard.jsx
+++ frontend/src/components/Dashboard/UserDashboard.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import {Col, Row} from "antd";
+import ReportBox from "./ReportBox";
+import {useUserDashboardData} from "../../hooks/useUserDashboardData";
 
 const UserDashboard = () => {
+  const { userDashboardData, userDashboardDataLoading } = useUserDashboardData();
+  const extractDataByOrderType = (data, orderType) => {
+    const orderData = {
+      firstValue: {
+        total: data.today.total,
+        amount: data.today.amount,
+      },
+      secondValue: {
+        total: data.monthly.total,
+        amount: data.monthly.amount,
+      },
+    };
+    return orderData;
+  }
   return (
-    <div>
-      This is user dashboard
-    </div>
+    <>
+      <Row gutter={[16, 16]}>
+        <Col xs={24} md={12} lg={6}>
+          <ReportBox
+            data={userDashboardData ? extractDataByOrderType(userDashboardData, 'order') : null}
+            loading={userDashboardDataLoading}
+            text="Order"
+          />
+        </Col>
+      </Row>
+    </>
   );
 };
 
